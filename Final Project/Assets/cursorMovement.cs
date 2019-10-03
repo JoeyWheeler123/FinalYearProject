ba82120cf593f65d786c2aaf8159acf0f68d0c12
Mark <c16765279@mydit.ie>
2019-10-03 16:22:38 +0100

    3D Cursor

---
+++ Final Project/Assets/cursorMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorMovement : MonoBehaviour
+{
+    private Camera cam;
+    Vector3 point = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(point.x,point.y,0);
+    }
+    
+    void OnGUI()
+    {
+        
+        Event   currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane+10));
+
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+//        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.EndArea();
+    }
+}
