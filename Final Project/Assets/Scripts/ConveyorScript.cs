10605ccb6882b58c03bd5361942e3d19c2002cb2
Mark <c16765279@mydit.ie>
2019-11-28 19:30:59 +0000

    elevator test

---
+++ Final Project/Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right*Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.transform.parent = (this.transform);
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        other.transform.SetParent(null);
+    }
+}
