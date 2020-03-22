8d5d502ca38a4e2aab053194d58ae11c276cd35c
Outain <c16765279@mydit.ie>
2020-03-22 02:29:14 +0000

    curosr imrprovment level tweak

---
+++ Final Project/Assets/CursorRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorRotator : MonoBehaviour
+{
+    public bool directionRotation;
+    public bool randomRotation;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randomRotation)
+        {
+            transform.Rotate(transform.up*rotateSpeed*Time.deltaTime);
+        }
+    }
+}
