3b7474842b2a106c7d0a6ad656b16bb314ee354d
Outain <c16765279@mydit.ie>
2020-04-11 22:01:04 +0100

    player now wakes up in first scene

---
+++ Final Project/Assets/InterruptableAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InterruptableAnimation : MonoBehaviour
+{
+    private PlayerControls controls;
+    public UnityEvent playerSleeping, returnControl;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.Jump.performed += ctx => Interrupt();
+        playerSleeping.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    void Interrupt()
+    {
+        print("iNTERRUPT");
+        anim.SetBool("Waking", true);
+        StartCoroutine(ReturnControl());
+    }
+
+    IEnumerator ReturnControl()
+    {
+        yield return new WaitForSeconds(12f);
+        returnControl.Invoke();
+        yield return null;
+    }
+}
