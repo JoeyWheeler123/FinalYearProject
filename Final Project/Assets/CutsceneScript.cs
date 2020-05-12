ad016462304d09b1f620164e9ed988aa9b552d8d
Outain <c16765279@mydit.ie>
2020-05-12 15:08:28 +0100

    level transitions and saving added

---
+++ Final Project/Assets/CutsceneScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CutsceneScript : MonoBehaviour
+{
+    public UnityEvent beginningEvent, middleEvent, outroEvent;
+    public float interval1, interval2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimedEvents());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator TimedEvents()
+    {
+        if (beginningEvent != null)
+        {
+            beginningEvent.Invoke();
+        }
+        yield return new WaitForSeconds(interval1);
+        if (middleEvent != null)
+        {
+            middleEvent.Invoke();
+        }
+        yield return new WaitForSeconds(interval2);
+        if (outroEvent != null)
+        {
+            outroEvent.Invoke();
+        }
+        yield return null;
+    }
+}
