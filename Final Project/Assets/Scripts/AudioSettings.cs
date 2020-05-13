36e0195b0ebc734fd85e57e2522f25ea1de0f3d9
Connor Ryan <c16755215@mydit.ie>
2020-05-13 16:22:49 +0100

    audi slider functionality main menu

---
+++ Final Project/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    FMOD.Studio.EventInstance SFXVolumeTest;
+
+    FMOD.Studio.Bus Music;
+    FMOD.Studio.Bus SFX;
+    FMOD.Studio.Bus Master;
+    float MusicVolume = 0.5f;
+    float SFXVolume = 0.5f;
+    float MasterVolume = 0.5f;
+
+    private void Awake()
+    {
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+        SFXVolumeTest = FMODUnity.RuntimeManager.CreateInstance("event:/2D_Events/UISounds2");
+    }
+
+    private void Update()
+    {
+        Music.setVolume(MusicVolume);
+        SFX.setVolume(SFXVolume);
+        Master.setVolume(MasterVolume);
+    }
+
+    public void MasterVolumeLevel (float newMasterVolume)
+    {
+        MasterVolume = newMasterVolume;
+
+        FMOD.Studio.PLAYBACK_STATE PbState;
+        SFXVolumeTest.getPlaybackState(out PbState);
+        if (PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            SFXVolumeTest.start();
+        }
+    }
+
+    public void MusicVolumeLevel(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+    }
+
+    public void SFXVolumeLevel(float newSFXVolume)
+    {
+        SFXVolume = newSFXVolume;
+
+        FMOD.Studio.PLAYBACK_STATE PbState;
+        SFXVolumeTest.getPlaybackState(out PbState);
+        if(PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            SFXVolumeTest.start();
+        }
+    }
+}
