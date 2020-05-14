e40779c7cecf8df17c3ed182161448b928a0e58c
Mark <c16765279@mydit.ie>
2020-05-14 18:15:18 +0100

    Added ending credits

---
+++ Final Project/Assets/CreditsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour
+{
+public CanvasGroup cGroup;
+public GameObject title,credits;
+public float timeToStart,titleTime,namesTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+    StartCoroutine(BeginCredits());
+        if(cGroup!=null){
+        cGroup = GetComponent<CanvasGroup>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator BeginCredits(){
+    yield return new WaitForSeconds(timeToStart);
+    float timeElapsed = 0f;
+            while(timeElapsed<=2f){
+            cGroup.alpha +=Time.deltaTime/2f;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+            }
+            title.SetActive(true);
+   yield return new WaitForSeconds(titleTime);
+   title.SetActive(false);
+   credits.SetActive(true);
+   yield return new WaitForSeconds(namesTime);
+    }
+}
+
